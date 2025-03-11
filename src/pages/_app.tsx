274a8ec390d39bdfc3cca19181244ed981d39cd5
Masaki Moriyama <masaquid@gmail.com>
2025-03-11 13:09:20 +0900

    SEO関連の設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,30 @@
+import Head from "next/head";
+import type { AppProps } from "next/app";
 import "@/styles/globals.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import { SITE_NAME, SITE_DESCRIPTION } from "@/config";
 
 config.autoAddCss = false;
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>{SITE_NAME}｜{SITE_DESCRIPTION}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Notion をデータソースに、独自ドメインで無料運営できるブログです。"
+        />
+        <meta
+          name="keywords"
+          content="Notion, Next.js, Vercel, ブログ, ヘッドレスCMS"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,15 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="ja">
+      <Head>
+        <meta name="robots" content="index,follow" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
